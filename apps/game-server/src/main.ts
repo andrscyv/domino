080b467bbd55eec8003f6093e7e801fbec648ff4
Andres Cruz <andrscyv@gmail.com>
2022-07-14 23:13:34 -0500

    feat(game-server):migrate domino game server

--- apps/game-server/src/main.ts
+++ apps/game-server/src/main.ts
@@ -1,1 +1,9 @@
-console.log('Hello World!');
+import { Origins, Server } from 'boardgame.io/server';
+import { DominoGame } from '@domino/domino';
+
+const server = Server({
+  games: [DominoGame],
+  origins: Origins.LOCALHOST_IN_DEVELOPMENT,
+});
+
+server.run(8000);
--- libs/domino/src/index.ts
+++ libs/domino/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/domino-client';
+export * from './lib/domino-game';
